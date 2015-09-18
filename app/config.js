2f06b89f3f91e6981c9c8fd8e608e18c12b1e453
Hack Reactor Students <accounts@hackreactor.com>
2015-09-17 19:03:57 -0700

    Add the productions database url

--- app/config.js
+++ app/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: 'mongodb://localhost:27017/shortlydb'
+  // url: 'mongodb://localhost:27017/shortlydb'
+  url: 'mongodb://MongoLab-BetterThanBitly:HackReactor@ds042698.mongolab.com:42698/MongoLab-BetterThanBitly'
 }
